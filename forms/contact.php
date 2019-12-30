83c3bbe5c99e9be97e05994e16f017610c338dab
unknown <wei.xw.xu@gmail.com>
2019-12-30 12:18:09 -0500

    updated contact form

--- forms/contact.php
+++ forms/contact.php
@@ -1,33 +1,20 @@
 <?php
-if(isset($_POST['email'])) {
- 
-    $email_to = "wei.xw.xu@gmail.com"; 
-    
+
+  if(isset($_POST['submit'])){
     $name = $_POST['name']; // required
     $email_from = $_POST['email']; // required
     $subject = $_POST['subject']; // required
     $message = $_POST['message']; // required
- 
-    $error_message = "";
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
- 
-    $email_message = "Form details below.\n\n";
- 
-     
-    function clean_string($string) {
-      $bad = array("content-type","bcc:","to:","cc:","href");
-      return str_replace($bad,"",$string);
-    }
- 
-     
-    $email_message .= "Subject: ".clean_string($subject)."\n";
-    $email_message .= "Name: ".clean_string($name)."\n";
-    $email_message .= "Email: ".clean_string($email_from)."\n";
-    $email_message .= "Message: ".clean_string($message)."\n";
- 
-// create email headers
-$headers = 'From: '.$email_from."\r\n".
-'Reply-To: '.$email_from."\r\n" .
-'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $subject, $email_message, $headers);  
+
+    //gmail won't work automatically
+    $email_to = "weixueternal@hotmail.com"; 
+    
+    $headers = "From: ".$email_from;
+    $txt = "You got a message from: ".$name.".\n\n".$message;
+
+    mail($email_to, $subject, $txt, $headers);
+
+    header("Location: contact.html?mailsend");
+  } 
+
 ?>
